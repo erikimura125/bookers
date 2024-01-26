6cf1c8fdaf47aaa1a8389573d1596fdf7da16d31
eri_kimura <erikimura125@gmail.com>
2024-01-26 14:50:05 +0000

    バリデーション設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,14 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -22,15 +27,22 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"  
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.find(params[:id])
+      render :edit
+    end
   end
   
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "successfully"
+      redirect_to '/books'
+    end
   end
 
   private
